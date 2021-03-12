f5754827b2439e2ffd2a5d7c45aac7950f3e2281
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-03-12 16:37:11 +0000

    Update - Repositories components

---
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+        <strong>{props.repository ?? 'Default'}</strong>
+        <p>Forms in React</p>
+
+        <a href="">
+            Access repository
+        </a>
+    </li>
+
+    )
+}
---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Repository List</h1>
+
+            <ul>
+                <RepositoryItem repository="unform2" description="Forms in React" link="https://github.com/unform/unform"/>
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
+            </ul>
+        </section>
+    );
+}
