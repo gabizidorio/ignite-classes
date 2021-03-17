ff14a9e05013be310571492e9711f44600e4b31f
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-03-17 12:31:52 +0000

    Update - Webpack Fast Refresh

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         hot: true,
     },
     plugins: [
-        new isDevelopment && new ReactRefreshWebpackPlugin(),
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
